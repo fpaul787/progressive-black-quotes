42b3ffb4b461007ff16186106ff5d91abf81fdae
Frantz <fpaul787@gmail.com>
2020-06-02 16:06:51 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+function changeSlide() {
+    // document.getElementById('pic').innerHTML = "pic"
+    document.getElementById('quote').innerHTML = "quote change"
+}
+
+document.getElementById('next').onclick = changeSlide;
