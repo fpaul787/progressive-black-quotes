026e009ac62cc5ff116f00375346a4e8beda7721
Frantz <fpaul787@gmail.com>
2020-06-03 23:54:50 -0400

    javascript file progress

--- js/script.js
+++ js/script.js
@@ -3,13 +3,36 @@
 
 
 function init(){
-    var section = document.getElementById('webslides');
-    if(section){
+    var webslides = document.getElementById('webslides');
+    if(webslides){
         // alert('Section loaded')
+        var section;
 
-        for(i=0; i<1; i++){
-            alert(i)
+        for(i=0; i<3; i++){
+            section = document.createElement('section');
+            section.innerHTML = `            
+                <div id="slider">
+                    <div id="pic">
+                        <img src="images/Malcolm-X.jpg" alt="Malcolm-X">
+                        <p id="creditBlock">Credit:</p>                   
+                    </div>                   
+                    <div>                    
+                        <p class="text-context" id="textBlock">
+                            "Lorem ipsum dolor sit amet consectetur,
+                            adipisicing elit. Doloribus laudantium
+                            beatae repellendus. Similique placeat
+                            amet pariatur ducimus eligendi asperiores
+                            dolor minus est quos natus. Voluptate."
+                            <br><br>
+                            From: Malcolm-X
+                        </p>
+                        
+                    </div>
+                </div>            
+            `
+            webslides.appendChild(section)
         }
+        
     }
 }
 
