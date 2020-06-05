226294360b74d255f9db1b870c796c49860f9a53
Frantz <fpaul787@gmail.com>
2020-06-04 22:12:53 -0400

    testing changes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'qoutes.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', '../qoutes.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -24,7 +24,9 @@ function intermediate(json){
 function loadJsonInit(){
     var data;
     loadJSON(function(response){
-        data = JSON.parse(response);        
+        // console.log(response)
+        data = JSON.parse(response);   
+        // console.log(data)     
         intermediate(data)
     })
 }
@@ -53,22 +55,22 @@ function createSlides(data){
         // alert('')
         var webslides = document.getElementById('webslides');   
         
-        // console.log(data[0])
+        console.log(data)
         var section;
-        for(i=0; i<3; i++){
+        for(i=0; i<data.length; i++){
             section = document.createElement('section');
             section.setAttribute('class', 'slide')
             section.innerHTML = `            
                 <div id="slider">
                     <div id="pic">
-                        <img src=${data[0].pic} alt="Malcolm-X">
-                        <p id="creditBlock">Credit: ${data[0].pic_credit}</p>                   
+                        <img src=${data[i].pic} alt="Malcolm-X">
+                        <p id="creditBlock">Credit: ${data[i].pic_credit}</p>                   
                     </div>                   
                     <div>                    
                         <p class="text-context" id="textBlock">
-                            "${data[0].quote}"
+                            "${data[i].quote}"
                             <br><br>
-                            From: ${data[0].name}
+                            From: ${data[i].name}
                         </p>                        
                     </div>
                 </div>            
